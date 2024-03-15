8cd39e8892f63253cd62bc20c21b1e09f23ea73d
oksana.misiura.cld@gmail.com <oksana.misiura.cld@gmail.com>
2024-03-15 22:33:03 +0000

    init

---
+++ src/models/json/track-json-store.js
@@ -0,0 +1,64 @@
+import { v4 } from "uuid";
+import { db } from "./store-utils.js";
+
+export const trackJsonStore = {
+  async getAllTracks() {
+    await db.read();
+    return db.data.tracks;
+  },
+
+  async addTrack(playlistId, track) {
+    await db.read();
+    track._id = v4();
+    track.playlistid = playlistId;
+    db.data.tracks.push(track);
+    await db.write();
+    return track;
+  },
+
+  async getTracksByPlaylistId(id) {
+    await db.read();
+    let foundTracks = db.data.tracks.filter((track) => track.playlistid === id);
+    if (!foundTracks) {
+      foundTracks = null;
+    }
+    return foundTracks;
+  },
+
+  async getTrackById(id) {
+    await db.read();
+    let foundTrack = db.data.tracks.find((track) => track._id === id);
+    if (!foundTrack) {
+      foundTrack = null;
+    }
+    return foundTrack;
+  },
+
+  async getPlaylistTracks(playlistId) {
+    await db.read();
+    let foundTracks = tracks.filter((track) => track.playlistid === playlistId);
+    if (!foundTracks) {
+      foundTracks = null;
+    }
+    return foundTracks;
+  },
+
+  async deleteTrack(id) {
+    await db.read();
+    const index = db.data.tracks.findIndex((track) => track._id === id);
+    if (index !== -1) db.data.tracks.splice(index, 1);
+    await db.write();
+  },
+
+  async deleteAllTracks() {
+    db.data.tracks = [];
+    await db.write();
+  },
+
+  async updateTrack(track, updatedTrack) {
+    track.title = updatedTrack.title;
+    track.artist = updatedTrack.artist;
+    track.duration = updatedTrack.duration;
+    await db.write();
+  },
+};
